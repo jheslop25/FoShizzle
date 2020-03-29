809af5f5e266f046709d16051a94961c71780e76
jheslop25 <john@earthmountain.ca>
2020-03-29 21:26:15 +0000

    worked on msg controller methods

--- FoShizzle/app/Http/Controllers/messageController.php
+++ FoShizzle/app/Http/Controllers/messageController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class messageController extends Controller
 {
-    //
+    public function create(Request $request){
+        $msg = new \App\Message();
+        $msg->user_id = Auth::user()->id;
+        $msg->rec_id = $request->recID;
+        $msg->msg = $request->msg;
+        $msg->save();
+        $sucess = 'success';
+        return response($sucess);
+    }
+
+    public function getMsg(Request $request){
+        //get all user messages
+    }
 }
