4df8789666bbf65e20600a660e23b0af8673995f
jheslop25 <john@earthmountain.ca>
2020-03-29 01:03:29 +0000

    finished remaining functions in tweet controller

--- FoShizzle/app/Http/Controllers/tweetController.php
+++ FoShizzle/app/Http/Controllers/tweetController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class tweetController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         //create a tweet
-        if(Auth::check()){
+        if (Auth::check()) {
             $tweet = new \App\Tweet();
             $tweet->user_id = Auth::user()->id;
             $tweet->content = $request->content;
@@ -16,28 +17,58 @@ class tweetController extends Controller
             $tweet->orig_tweet_id = $request->tweetID;
             $tweet->photo_url = $request->photo;
 
-            if($tweet->save()){
+            if ($tweet->save()) {
                 $msg = "success! you created a tweet";
                 return response($msg);
             }
-
-
         }
     }
 
-    public function update(Request $request){
-       //update a tweet
+    public function update(Request $request)
+    {
+        //update a tweet
+        if (Auth::check()) {
+            $tweet = \App\Tweet::find($request->id);
+            $tweet->user_id = Auth::user()->id;
+            $tweet->content = $request->content;
+            $tweet->orig_user_id = $request->origUser;
+            $tweet->orig_tweet_id = $request->tweetID;
+            $tweet->photo_url = $request->photo;
+
+            if ($tweet->save()) {
+                $msg = "success! you updated a tweet";
+                return response($msg);
+            }
+        }
     }
 
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
         //delete a tweet
+        if(Auth::check()){
+            \App\Tweet::find($request->id)->delete();
+            $msg = "you deleted a tweet";
+            return response($msg);
+        }
+
     }
 
-    public function get(Request $request){
+    public function get(Request $request)
+    {
         //get a single tweet
+        if (Auth::check()){
+            $tweet = \App\Tweet::find($request->id);
+            return response()->json($tweet);
+        }
     }
 
-    public function getAll(Request $request){
+    public function getAll(Request $request)
+    {
         //get all tweets and order by date created
+        if (Auth::check()){
+            $tweet = new \App\Tweet;
+            $tweets = $tweet->all();
+            return response()->json($tweets);
+        }
     }
 }
