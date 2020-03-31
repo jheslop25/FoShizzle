3aefccd959d778a41357d488a832e36a5f94067c
jheslop25 <john@earthmountain.ca>
2020-03-31 23:49:59 +0000

    worked on profileController functions

--- FoShizzle/app/Http/Controllers/profileController.php
+++ FoShizzle/app/Http/Controllers/profileController.php
@@ -3,18 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class profileController extends Controller
 {
     public function getUser(Request $request){
         //get user info for profile component
+        if(Auth::check()){
+            $id = $request->id;
+            $user = \App\User::find($id);
+
+            return response()->json(['user' => $user]);
+        }
     }
 
     public function update(Request $request){
         //update a user profile
+        if(Auth::user()->id == $request->id){
+            $user = \App\User::find(Auth::user()->id);
+            $user->id = Auth::user()->id;
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->profile_pic = $request->photo;
+            if($user->save()){
+                $msg = 'user updated';
+                return response($msg);
+            }
+        }
     }
 
     public function destroy(Request $request){
         //destroy a user, their tweets, and comments etc
+        if (Auth::user()->id == $request->id){
+            $user = Auth::user();
+            $id = Auth::user()->id;
+            \App\Tweet::where('user_id', $id)->delete();
+            \App\Comment::where('user_id', $id)->delete();
+            \App\Follow::where('user_id', $id)->delete();
+            \App\Message::where('user_id', $id)->delete();
+
+            return view('home');
+        }
     }
 }
