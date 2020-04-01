2a4cf916bebe0c8fdcfd32f00b69b32a05ecb91d
jheslop25 <john@earthmountain.ca>
2020-04-01 00:03:19 +0000

    worked on getMsg function in message controller. returns a json object that is essentially an array of messages. this needs to be upgraded later to return a more complicated data structure that includes usernames of both parties etc.

--- FoShizzle/app/Http/Controllers/messageController.php
+++ FoShizzle/app/Http/Controllers/messageController.php
@@ -18,5 +18,15 @@ class messageController extends Controller
 
     public function getMsg(Request $request){
         //get all user messages
+        $convo = [];
+        $msgSent = \App\Message::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+        $msgRec = \App\Message::where('rec_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
+
+        for($i = 0; $i < sizeof($msgSent + 1); $i++){
+            array_push($convo, $msgSent[$i]);
+            array_push($convo, $msgRec[$i]);
+        }
+
+        return response()->json(['convo' => $convo]);
     }
 }
