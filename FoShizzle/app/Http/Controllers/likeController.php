d1daff28402ad6359355df80107b95e1cc236e07
jheslop25 <john@earthmountain.ca>
2020-04-01 00:12:35 +0000

    filled in like and unlike functions in likeController

--- FoShizzle/app/Http/Controllers/likeController.php
+++ FoShizzle/app/Http/Controllers/likeController.php
@@ -8,17 +8,37 @@ class likeController extends Controller
 {
     public function likeTweet(Request $request){
         //like a tweet
+        if(Auth::check()){
+            $like = new \App\Like();
+            $like->tweet_id = $request->likeTweet;
+            $like->user_id = Auth::user()->id;
+            if($like->save()){
+                return response()->json(['status' => 200]);
+            }
+        }
     }
 
     public function unlikeTweet(Request $request){
         //unlike a tweet
+        if(Auth::check()){
+            if(\App\Tweet::find($request->tweetId)->delete()){
+                return response()->json(['status'=>200]);
+            }
+
+        }
     }
 
     public function likeComment(Request $request){
         //like a comment
+        if(Auth::check()){
+
+        }
     }
 
     public function unlikeComment(Request $request){
         //unlike a comment
+        if(Auth::check()){
+
+        }
     }
 }
